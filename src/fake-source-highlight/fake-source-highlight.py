3bc3c1e4b6a45ddf5ac3e162dea030d3975443fc
Lance Gatlin <lance.gatlin@tstllc.net>
2014-05-27 16:45:27 -0400

    modified fake-source-highlight to emi class tag recognized by wordpress crayon plugin. propagated changes in asciidocs. converted more posts

--- src/fake-source-highlight/fake-source-highlight.py
+++ src/fake-source-highlight/fake-source-highlight.py
@@ -2,12 +2,31 @@
 
 # Fake for source-highlight that wraps stdin in <pre></pre> block only. To use, uninstall gnu source-highlighter and copy this to path.
 
-import sys
+import sys, getopt
 
-write = sys.stdout.write
-write('<pre>')
-content = sys.stdin.read()
-content = content.rstrip('\n')
-write(content)
-write('</pre>')
-sys.exit(0)
+def main(argv):
+  lang=''
+
+  try:
+    opts, args = getopt.getopt(argv,"f:s:",["line-number="])
+  except getopt.GetoptError:
+    print 'source-highlight -f <ignored> -s <source language> --line-number <ignored>'
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt == '-s':
+      lang = arg
+
+  if lang == '':
+    print 'lang not set!'
+    sys.exit(2)
+
+  write = sys.stdout.write
+  write('<pre class="lang:{lang}">'.format(lang=lang))
+  content = sys.stdin.read()
+  content = content.rstrip('\n')
+  write(content)
+  write('</pre>')
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
